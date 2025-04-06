aa98606338369ec6b90acfc4b5c8027eb4903775
Mikhail <mrus7684@gmail.com>
2025-04-06 15:20:40 +0300

    T1

---
+++ src/main/java/com/project/smartfamily/SmartFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.project.smartfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFamilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFamilyApplication.class, args);
+    }
+
+}
